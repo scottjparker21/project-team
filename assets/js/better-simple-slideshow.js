7793571f561a78ac20cc280e860c3458ad89dac4
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-10-30 22:20:10 -0500

    added alt tags and comments where missing and moved javascript references to bottom of pages

--- assets/js/better-simple-slideshow.js
+++ assets/js/better-simple-slideshow.js
@@ -159,3 +159,20 @@ var makeBSS = function (el, options) {
         $slideshow.init(el, options);
     });
 };
+
+        var opts = {
+            auto : {
+                speed : 3500,
+                pauseOnHover : true
+            },
+            fullScreen : false,
+            swipe : true
+        };
+        makeBSS('.num1', opts);
+
+        var opts2 = {
+            auto : false,
+            fullScreen : true,
+            swipe : true
+        };
+        makeBSS('.num2', opts2);
